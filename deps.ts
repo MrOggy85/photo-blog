d8a822aa5a8f7e6e0d76a5b5f4e694367b848858
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-10-11 05:13:19 +0900

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export { CSS } from "https://deno.land/x/gfm@0.1.20/mod.ts";
+export { CSS } from "https://deno.land/x/gfm@0.1.26/mod.ts";
 export {
   Application,
   Context,
   Router,
-} from "https://deno.land/x/oak@v10.6.0/mod.ts";
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-export type { RouterContext } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v11.1.0/mod.ts";
