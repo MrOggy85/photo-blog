9f73b3307b263ea54b4fc03c6e24d7591a2c9add
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-10-11 10:01:18 +0900

    feat: add keyboard support

---
+++ static/script.js
@@ -0,0 +1,79 @@
+const WIDE = 'wide'
+const FILL = 'fill'
+const KEY_ESCAPE = 'Escape'
+const KEY_ARROW_RIGHT = 'ArrowRight';
+const KEY_ARROW_LEFT = 'ArrowLeft';
+
+let currentModalImgSrc = ''
+
+function onSelectChange(e) {
+  document.querySelector('main').classList.remove(WIDE);
+  document.querySelector('main').classList.remove(FILL);
+
+  const v = e.target.value;
+  if (v === WIDE) {
+    document.querySelector('main').classList.add(WIDE);
+  }
+  if (v === FILL) {
+    document.querySelector('main').classList.add(FILL);
+  }
+}
+
+function onImgClick(e) {
+  const src = e.target.src;
+  document.body.classList.add('lock');
+  updateModalImg(src);
+}
+
+function closeModal() {
+  document.body.classList.remove('lock');
+}
+
+function next() {
+  const currentImg = document.querySelector(`[src="${currentModalImgSrc}"]`);
+  const nextSibling = currentImg.nextSibling;
+
+  const src = nextSibling ? nextSibling.src : document.querySelector('.photos img').src;
+
+  updateModalImg(src);
+}
+function previous() {
+  const currentImg = document.querySelector(`[src="${currentModalImgSrc}"]`);
+  const previousSibling = currentImg.previousSibling;
+
+  let src = previousSibling ? previousSibling.src : '';
+  if (!src) {
+    const all = document.querySelectorAll('.photos img')
+    src = all[all.length - 1].src;
+  }
+
+  updateModalImg(src);
+}
+
+function updateModalImg(src) {
+  currentModalImgSrc = src;
+  document.querySelector('.modal img').src = src;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.view select').oninput = onSelectChange;
+  for (const elt of document.querySelectorAll('img')) {
+    elt.onclick = onImgClick;
+  }
+  document.querySelector('.modal').onclick = closeModal;
+  document.body.addEventListener('keyup', (e) => {
+    if (e.key === KEY_ESCAPE) {
+      closeModal();
+    }
+    if (document.body.classList.contains('lock')) {
+      console.log('modal is showing!');
+      if (e.key === KEY_ARROW_RIGHT) {
+        next();
+      }
+      if (e.key === KEY_ARROW_LEFT) {
+        previous();
+      }
+    }
+
+  });
+});
